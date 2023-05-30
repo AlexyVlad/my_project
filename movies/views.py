c242e8cd8c133d0f17fa838236fdf281f51e7135
alexy <vladialex@inbox.ru>
2023-05-30 18:42:50 +0400

    Поиск по фильмам и немного css к детейлу фильма

--- movies/views.py
+++ movies/views.py
@@ -2,6 +2,8 @@ from django.views.generic import ListView, DetailView, View
 from django.shortcuts import render
 from movies.models import Movie
 from django.shortcuts import render, redirect
+
+from . import filters
 from .forms import ReviewForm
 
 
@@ -12,6 +14,17 @@ def index(request):
 class MoviesListView(ListView):
     model = Movie
     template_name = "movie_list.html"
+
+    def get_filters(self):
+        return filters.MovieNameFilter(self.request.GET)
+
+    def get_queryset(self):
+        return self.get_filters().qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = self.get_filters()
+        return context
 
 
 class MovieDetailView(DetailView):
